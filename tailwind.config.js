4b0e5131fdf69bf4dcd3c6612b6a21c86891cb6a
afifudinn <work.afifudin@gmail.com>
2021-11-28 21:13:42 +0700

    fix wrong place to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
       fontFamily: {
-        'cool': ['Inter', 'sans-serif']
-      }
+        cool: ["Inter", "sans-serif"],
+      },
     },
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
