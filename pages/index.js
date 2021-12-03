01b9e3ff547b70ad7b83af6231cc41602baeda37
afifudinn <work.afifudin@gmail.com>
2021-12-03 19:21:19 +0700

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,22 @@
 import Head from "next/head";
-import Link from "next/link";
+import GlobalLayout from "@components/GlobalLayout";
+import Layout from "@components/Layout";
+import Navbar from "@components/Navbar";
 export default function Home() {
   return (
-    <div className="font-cool font-semibold text-base sm:text-xl">
+    <GlobalLayout>
       <Head>
         <title>LONDRY | One tap order</title>
       </Head>
-      <div className="fixed w-screen px-8 sm:px-32 py-4 sm:py-6 flex justify-between">
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <a>Login</a>
-      </div>
-      <div className="px-8 sm:px-32 py-4 min-h-screen min-w-screen flex flex-wrap justify-between items-center content-center bg-gradient-to-br from-white to-blue-300">
-        <div>
-          <div className="flex justify-start items-end">
-            <p className="font-black text-8xl sm:text-9xl">
-              LON
-              <br />
-              DRY
-            </p>
-            <img
-              src="/images/char.png"
-              alt="char"
-              className="max-h-48 sm:max-h-72"
-            />
-          </div>
-          <p className="text-xl sm:text-3xl font-bold">
-            Laundry just in couple of click
-          </p>
-        </div>
-        <div className="mt-16 xl:mt-0">
+      <Navbar />
+      <Layout>
+        <div className="w-full mt-16 xl:mt-0">
           <p>Let's proceed your order 🎉</p>
-          <div className="max-w-2xl grid gap-2 sm:gap-4 grid-cols-2 my-4 xl:my-8">
+          <div className="grid gap-2 sm:gap-4 grid-cols-2 my-4 xl:my-8">
             <input
               type="text"
               placeholder="Address e.g Jalan No.5 Imam Ghazali"
-              className="col-span-2 w-full border-2 sm:border-4 outline-none focus:outline-none focus:border-blue-300 active:outline-none p-2 sm:p-6 font-semibold rounded-lg sm:rounded-2xl text-xs sm:text-base md:text-xl"
+              className="block col-span-2 w-full border-2 sm:border-4 outline-none focus:outline-none focus:border-blue-300 active:outline-none p-2 sm:p-6 font-semibold rounded-lg sm:rounded-2xl text-xs sm:text-base md:text-xl"
             />
             <input
               type="text"
@@ -57,7 +37,7 @@ export default function Home() {
             *We'll come to your home for estimated time of 15 minutes
           </p>
         </div>
-      </div>
-    </div>
+      </Layout>
+    </GlobalLayout>
   );
 }
