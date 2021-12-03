c348fd925fc30078c92b8d04059a4c391ce63f71
afifudinn <work.afifudin@gmail.com>
2021-12-03 19:20:48 +0700

    adding Global Layout

---
+++ components/GlobalLayout.js
@@ -0,0 +1,7 @@
+export default function GlobalLayout({ children }) {
+  return (
+    <div className="font-cool font-semibold text-base sm:text-xl bg-gradient-to-br from-white to-blue-300">
+      {children}
+    </div>
+  );
+}
